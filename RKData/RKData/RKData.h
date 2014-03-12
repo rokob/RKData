221a5cec0d8cb00df89383bcfe8e0a33e444119e
Andrew Ledvina <led@fb.com>
2014-03-12 01:57:45 -0700

    Playing around with some ideas

--- RKData/RKData/RKData.h
+++ RKData/RKData/RKData.h
@@ -3,5 +3,70 @@
 #import <Foundation/Foundation.h>
 
 @interface RKData : NSObject
+@end
+
+@protocol RKCache <NSObject>
+- (id)objectForKey:(id)key;
+- (void)setObject:(id)object forKey:(id)key;
+@end
+
+@interface RKEntity : NSObject
+@end
+
+@protocol RKEntityDescription <NSObject>
+- (NSURL *)URL;
+@end
+
+@protocol RKEntityValue <NSObject>
+- (RKEntity *)value;
+@end
+
+@protocol RKSubscriptionHandle <NSObject>
+- (void)remove;
+@end
+
+@protocol RKEntityParser <NSObject>
+- (id<RKEntityValue>)valueFromObject:(id)object withDescription:(id<RKEntityDescription>)description;
+@end
+
+typedef NS_OPTIONS(NSUInteger, RKSubscriptionEvent) {
+  RKSubscriptionEventCreated = 1 << 0,
+  RKSubscriptionEventValueOnce = 1 << 1,
+  RKSubscriptionEventDelete = 1 << 2,
+  RKSubscriptionEventValue = 1 << 3 | 1 << 2 | 1 << 1 | 1 << 0,
+};
+
+typedef NS_ENUM(NSUInteger, RKDataSubscriberPolicy) {
+  RKDataSubscriberPolicyDefault,
+  RKDataSubscriberPolicyCacheOnly,
+};
+
+@interface RKDataSubscriber : NSObject
+
+- (instancetype)initWithCache:(id<RKCache>)cache;
+- (instancetype)initWithCache:(id<RKCache>)cache baseURL:(NSURL *)baseURL;
+- (instancetype)initWithCache:(id<RKCache>)cache baseURL:(NSURL *)baseURL policy:(RKDataSubscriberPolicy)policy;
+
+- (id<RKSubscriptionHandle>)subscribeToEntity:(id<RKEntityDescription>)entity
+                                     callback:(void(^)(id<RKEntityValue> value))callback
+                                        queue:(dispatch_queue_t)queue;
+
+- (id<RKSubscriptionHandle>)subscribeToEntity:(id<RKEntityDescription>)entity
+                                        event:(RKSubscriptionEvent)event
+                                     callback:(void(^)(id<RKEntityValue> value))callback
+                                        queue:(dispatch_queue_t)queue;
+
+- (id<RKSubscriptionHandle>)subscribeToEntity:(id<RKEntityDescription>)entity
+                                       parser:(id<RKEntityParser>)parser
+                                     callback:(void(^)(id<RKEntityValue> value))callback
+                                        queue:(dispatch_queue_t)queue;
+
+- (id<RKSubscriptionHandle>)subscribeToEntity:(id<RKEntityDescription>)entity
+                                       parser:(id<RKEntityParser>)parser
+                                        event:(RKSubscriptionEvent)event
+                                    callback:(void(^)(id<RKEntityValue> value))callback
+                                       queue:(dispatch_queue_t)queue;
+
+- (void)removeAllSubscriptions;
 
 @end
--- RKData/RKData/RKData.m
+++ RKData/RKData/RKData.m
@@ -3,5 +3,206 @@
 #import "RKData.h"
 
 @implementation RKData
+@end
+
+@implementation RKEntity
+@end
+
+@class RKSubscription;
+
+@protocol RKSubscriptionDelegate <NSObject>
+- (void)removeSubscription:(RKSubscription *)subscription;
+@end
+
+@interface RKSubscription : NSObject <RKSubscriptionHandle>
+- (id)initWithEntity:(id<RKEntityDescription>)entity
+              parser:(id<RKEntityParser>)parser
+               event:(RKSubscriptionEvent)event
+            callback:(void(^)(id<RKEntityValue>))callback
+               queue:(dispatch_queue_t)queue
+            delegate:(id<RKSubscriptionDelegate>)delegate;
+@end
+
+@implementation RKSubscription
+{
+  id<RKEntityDescription> _entity;
+  id<RKEntityParser> _parser;
+  RKSubscriptionEvent _event;
+  void(^_callback)(id<RKEntityValue>);
+  dispatch_queue_t _queue;
+  id<RKSubscriptionDelegate> __weak _delegate;
+}
+
+- (id)initWithEntity:(id<RKEntityDescription>)entity
+              parser:(id<RKEntityParser>)parser
+               event:(RKSubscriptionEvent)event
+            callback:(void (^)(id<RKEntityValue>))callback
+               queue:(dispatch_queue_t)queue
+            delegate:(id<RKSubscriptionDelegate>)delegate
+{
+  if ((self = [super init])) {
+    _entity = entity;
+    _parser = parser;
+    _event = event;
+    _callback = [callback copy];
+    _queue = queue;
+    _delegate = delegate;
+  }
+  return self;
+}
+
+- (void)remove
+{
+  [_delegate removeSubscription:self];
+}
+
+- (void)performCallbackForEvent:(RKSubscriptionEvent)event withObject:(id)object
+{
+  if (event & _event) {
+    id<RKEntityValue> value = [_parser valueFromObject:object withDescription:_entity];
+    dispatch_async(_queue, ^{
+      if (_callback) {
+        _callback(value);
+      }
+    });
+  }
+}
+@end
+
+@interface RKEntityValue : NSObject <RKEntityValue>
+- (id)initWithObject:(id)object;
+@end
+
+@implementation RKEntityValue
+{
+  id _object;
+}
+
+- (id)initWithObject:(id)object
+{
+  if ((self = [super init])) {
+    _object = [object isEqual:[NSNull null]] ? [NSNull null] : object;
+  }
+  return self;
+}
+
+- (RKEntity *)value
+{
+  return _object;
+}
+@end
+
+@interface RKEntityParser : NSObject <RKEntityParser>
++ (instancetype)defaultParser;
+@end
+
+@implementation RKEntityParser
+
++ (instancetype)defaultParser
+{
+  return [[self alloc] init];
+}
+
+- (id<RKEntityValue>)valueFromObject:(id)object withDescription:(id<RKEntityDescription>)description
+{
+  return [[RKEntityValue alloc] initWithObject:object];
+}
+@end
+
+@interface RKDataSubscriber () <RKSubscriptionDelegate>
+{
+  id<RKCache> _cache;
+  NSURL *_baseURL;
+  RKDataSubscriberPolicy _policy;
+  NSHashTable *_subscriptions;
+}
+
+@end
+
+@implementation RKDataSubscriber
+
+- (id)initWithCache:(id<RKCache>)cache
+{
+  return [self initWithCache:cache baseURL:nil];
+}
+
+- (id)initWithCache:(id<RKCache>)cache baseURL:(NSURL *)baseURL
+{
+  return [self initWithCache:cache baseURL:baseURL policy:RKDataSubscriberPolicyDefault];
+}
+
+- (id)initWithCache:(id<RKCache>)cache baseURL:(NSURL *)baseURL policy:(RKDataSubscriberPolicy)policy
+{
+  if ((self = [super init])) {
+    _cache = cache;
+    _baseURL = baseURL;
+    _policy = policy;
+
+    NSPointerFunctionsOptions options = NSPointerFunctionsObjectPointerPersonality|NSPointerFunctionsStrongMemory;
+    _subscriptions = [[NSHashTable alloc] initWithOptions:options
+                                                 capacity:0];
+  }
+  return self;
+}
+
+- (id<RKSubscriptionHandle>)subscribeToEntity:(id<RKEntityDescription>)entity
+                                     callback:(void(^)(id<RKEntityValue>))callback
+                                        queue:(dispatch_queue_t)queue
+{
+  return [self subscribeToEntity:entity
+                          parser:[RKEntityParser defaultParser]
+                           event:RKSubscriptionEventValue
+                        callback:callback
+                           queue:queue];
+}
+
+- (id<RKSubscriptionHandle>)subscribeToEntity:(id<RKEntityDescription>)entity
+                                        event:(RKSubscriptionEvent)event
+                                     callback:(void(^)(id<RKEntityValue>))callback
+                                        queue:(dispatch_queue_t)queue
+{
+  return [self subscribeToEntity:entity
+                          parser:[RKEntityParser defaultParser]
+                           event:event
+                        callback:callback
+                           queue:queue];
+}
+
+- (id<RKSubscriptionHandle>)subscribeToEntity:(id<RKEntityDescription>)entity
+                                       parser:(id<RKEntityParser>)parser
+                                     callback:(void(^)(id<RKEntityValue>))callback
+                                        queue:(dispatch_queue_t)queue
+{
+  return [self subscribeToEntity:entity
+                          parser:parser
+                           event:RKSubscriptionEventValue
+                        callback:callback
+                           queue:queue];
+}
+
+- (id<RKSubscriptionHandle>)subscribeToEntity:(id<RKEntityDescription>)entity
+                                       parser:(id<RKEntityParser>)parser
+                                        event:(RKSubscriptionEvent)event
+                                     callback:(void(^)(id<RKEntityValue>))callback
+                                        queue:(dispatch_queue_t)queue
+{
+  RKSubscription *handle = [[RKSubscription alloc] initWithEntity:entity
+                                                           parser:parser
+                                                            event:event
+                                                         callback:callback
+                                                            queue:queue
+                                                         delegate:self];
+  return handle;
+}
+
+- (void)removeAllSubscriptions
+{
+  [_subscriptions removeAllObjects];
+}
+
+- (void)removeSubscription:(RKSubscription *)subscription
+{
+  [_subscriptions removeObject:subscription];
+}
 
 @end
